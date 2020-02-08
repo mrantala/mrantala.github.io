821662559367806f5b729b4993c455ffa1c05b7f
Matt Rantala <MapRantala@Gmail.com>
2020-02-08 08:38:44 -0600

    Add "respondWith" functon to deal with 200 when offline!

--- serviceworker.js
+++ serviceworker.js
@@ -25,4 +25,10 @@ self.addEventListener("install", installEvent => {
 
 self.addEventListener('fetch', function(event) {
  console.log(event.request.url);
+ 
+  event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
 });
