f8418e35f595e8ce1a208a551b4b06482a583e60
Matt Rantala <MapRantala@Gmail.com>
2020-02-08 11:21:37 -0600

    Renamed the Service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const staticDevCoffee = "dev-coffee-site-v1"
+const cacheName = "map-pwa1"
 const assets = [
   "/",
   "/index.html",
@@ -17,7 +17,7 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(cacheName).then(cache => {
       cache.addAll(assets)
     })
   )
