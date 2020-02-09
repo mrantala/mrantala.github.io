2ab310f630e57e919fc04815980cd1d54497f1bc
Matt Rantala <MapRantala@Gmail.com>
2020-02-08 19:31:02 -0600

    fix sw

--- js/doNew.js
+++ js/doNew.js
@@ -1,3 +1,3 @@
 function doNew(){
-  console.log("old");
+  console.log("old2");
 }
--- serviceworker.js
+++ serviceworker.js
@@ -7,7 +7,7 @@ const assets = [
   "/js/app.js",
   "/images/coffee1.jpg",
   "/images/coffee2.jpg",
-  "/images/coffee3.jpg"
+  "/images/coffee3.jpg",
   "/images/coffee4.jpg",
   // "/images/coffee5.jpg",
   // "/images/coffee6.jpg",
@@ -39,74 +39,3 @@ self.addEventListener('fetch', function(event) {
         )
     );        
 });
-
-/* self.addEventListener('fetch', function(event) {
-    console.log("SELF");
-    
-    try {
-        // console.log(" START XXXXXXXXXXXXXXXXX");
-        console.log(event.request);
-        // console.log(fetch(event.request));
-        // console.log(caches);
-        // console.log(" END XXXXXXXXXXXXXXXXXX");
-    } catch(err) {
-        console.log(err.message);
-    }
-                    var z = fetch(event.request);
-                    console.log(z);
-                    
-    if (event.request.url.includes("app.js")){ console.log("section 1");
-         event.respondWith(
-            fetch(event.request).then(function(response){
-              console.log(response);
-              if (response){
-                return response;
-              }
-              caches.match(event.request)
-                .then(function(response) {
-                    console.log("response 2!");
-                    console.log(response);
-                    return response;
-                })
-            })
-         )
-        // );
-    } else {console.log("section 2");
-        event.respondWith(
-            caches.match(event.request)
-                .then(function(response) {  
-                    if (response) {
-                        console.log("response 2!");
-                        return response;
-                    }
-                    console.log("fetch 2!");
-                    return fetch(event.request);
-         //return response || fetch(event.request);
-                }
-            )
-        );        
-    }
-}); */
-
-// self.addEventListener('fetch', function(event) {
- // console.log(event.request.url);
- 
-  // event.respondWith(
-   // caches.match(event.request).then(function(response) {
-/*      console.log("START ##############");
-     console.log(event);
-     console.log(response);
-     
-     if (response){
-        console.log("Response!");
-        console.log(caches);
-        // //caches.put(event.request,response.clone());
-        console.log(response.clone());
-        
-     } else { console.log("No Response!")};
-     
-     console.log("END ##############"); */
-     // return response || fetch(event.request);
-   // })
- // );
-// });
