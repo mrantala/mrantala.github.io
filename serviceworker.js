1f995d141268e704cf53990109459b7df5d5d1dd
Matt Rantala <MapRantala@Gmail.com>
2020-02-12 23:09:47 -0600

    6f

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const cacheName = "sw_0.0.6e";
+const cacheName = "sw_0.0.6f";
 console.log(cacheName);
 const assets = [
   "/",
@@ -15,8 +15,8 @@ self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(cacheName).then(cache => {
       cache.addAll(assets);
-    });
-  );
+    })
+  )
 });
 
 function clearOldCaches(){
