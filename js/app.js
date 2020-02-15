24da5f1d43c890dfbf7463fc78d9b361d0355c8b
Matt Rantala <MapRantala@Gmail.com>
2020-02-15 08:10:30 -0600

    Hearer Part 2

--- js/app.js
+++ js/app.js
@@ -2,14 +2,6 @@ let newWorker;
 const container = document.querySelector(".container")
 const coffees = [
   { name: "One", image: "images/coffee1.jpg" },
-  // { name: "Two", image: "images/coffee2.jpg" },
-  // { name: "Three", image: "images/coffee5.jpg" },
-  // { name: "Rchitecto", image: "images/coffee4.jpg" },
-  // { name: " Beatae", image: "images/coffee5.jpg" },
-  // { name: " Vitae", image: "images/coffee6.jpg" },
-  // { name: "Inventore", image: "images/coffee7.jpg" },
-  // { name: "Veritatis", image: "images/coffee8.jpg" },
-  // { name: "Accusantium", image: "images/coffee9.jpg" },
 ]
 
 const showCoffees = () => {
@@ -26,7 +18,6 @@ const showCoffees = () => {
               `)
   )
   container.innerHTML = output
-  doNew();
 }
 
 document.addEventListener("DOMContentLoaded", showCoffees);
