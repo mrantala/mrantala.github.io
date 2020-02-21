b14118c1c5b1643df8b3dbbb5e802fade12e2532
Matt Rantala <MapRantala@Gmail.com>
2020-02-20 21:42:54 -0600

    Moved Settings, the add weights adds ereal data now

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
 console.log("app.js");
-var dbName = "wwwDB";
+var dbName = "www_weights";
 var dbVersion = 1;
 var settingsDBName = "settings";
 var weightsDBName = "weights";
@@ -36,18 +36,27 @@ function createDB(){
       autoclose: true
     });
     
-$('#example1').DataTable({
+$('#weightTable').DataTable({
     "lengthMenu": [31,100,180,365]
 });
-$("#addWeight").on('click', function (event){
+$("#addRecord").on('click', function (event){
     var dbOpenRequest = window.indexedDB.open(dbName,1);
     
     dbOpenRequest.onsuccess = function(event) {
             var db = event.target.result;
             var transaction = db.transaction("weights","readwrite");
             var weightStore = transaction.objectStore(weightsDBName);
-            // var thisEntry = {"guid":generateUUID(),"date":"20200308","weight":"169.6","units":"lb"};
-            console.log(thisEntry);
+            var theWeight = $("#inputWeight")[0].value;
+            var theUnits = "unk";
+            if ($("#lbUnits")[0].checked === true){
+                theUnits = "lb";
+            } else {
+                if ($("#kgUnits")[0].checked === true){
+                    theUnits = "kg";
+                }
+            }
+            var theDate = $("#datepicker")[0].value; 
+            var thisEntry = {"guid":generateUUID(),"date":theDate,"weight":theWeight,"units":theUnits};
             weightStore.add(thisEntry);
         
     }
--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const cacheName = "WwW_0.0.3";
+const cacheName = "WwW_0.0.4";
 console.log(cacheName);
 const assets = [
   "/",
