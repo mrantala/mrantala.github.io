cdc35ed12bc1aa22b4e50d1fb0a5da59668bfcd0
mrantala <sisu42@proton.me>
2026-02-18 22:14:05 -0600

    v0.0.11: Trying to disable service worker

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ initRouter();
 initEntries();
 initSettings();
 
-// if ("serviceWorker" in navigator) {
-  // navigator.serviceWorker.getRegistrations().then(regs => {
-    // regs.forEach(reg => reg.unregister());
-  // });
-//}
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    regs.forEach(reg => reg.unregister());
+  });
+}
 // if ("serviceWorker" in navigator) {
   // navigator.serviceWorker.register("./sw.js");
 // }
